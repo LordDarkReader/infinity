9cdb842190be50984268724cbfb2b3c6c5f4bf52
Cezary.Polosak <cezary.polosak@plus.pl>
2021-02-02 20:03:54 +0100

    initial commit

---
+++ src/test/java/com/chak/infinity/infinity/InfinityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chak.infinity.infinity;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+class InfinityApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
